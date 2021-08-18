55104083017f0bd8b6ba26adc2730318b53d9a51
Jack Rigan <riganjack40@gmail.com>
2021-08-18 17:52:03 -0600

    fixed connection issue in knexfile and server.js

--- src/server.js
+++ src/server.js
@@ -3,6 +3,12 @@ const {PORT = 5000} = process.env;
 const app = require('./app');
 const knex = require('./db/connection');
 
+
+
 const listener = () => console.log(`Listening on port ${PORT}`);
+
+
+
+
 
 app.listen(PORT, listener)
